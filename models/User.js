84e68d389b690a4c5334a7e7478dbe360c761776
Sharashena Chairani <shenachairani@gmail.com>
2022-06-28 11:01:44 +0700

    user

--- models/User.js
+++ models/User.js
@@ -1,16 +1,12 @@
+
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
-    {
-        username:{ type: String, required: true, unique: true},
-        email: { type: String, required: true},
-        password:{ type: String, required: true},
-        isAdmin:{
-            type: Boolean, 
-            default: false,
-        },
-    },
-    {   timestamps: true}
+  {
+    username: { type: String, required: true, unique: true, min: 4, max: 20},
+    password: { type: String, required: true, min: 6},
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
